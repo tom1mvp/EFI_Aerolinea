8a0c63a276c6e628550a71fa7a9b35f575e60b3f
Thomas Elser Bratik <thomasbratik83@gmail.com>
2025-06-13 21:13:11 -0300

    Init project

---
+++ flights_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FlightsManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'flights_management'
---
+++ passengers_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PassengersManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'passengers_management'
